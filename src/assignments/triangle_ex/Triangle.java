c1709707613fe44e3f865494a7ca56b054937fc6
rachelsmithcode <Rachelcodesmith@outlook.com>
2016-09-09 14:59:15 +0100

    first test passing - print one star

---
+++ src/assignments/triangle_ex/Triangle.java
@@ -0,0 +1,12 @@
+package assignments.triangle_ex;
+
+/**
+ * Created by RachelSmith on 09/09/2016.
+ */
+public class Triangle {
+
+    public String justOneStar() {
+        return "*";
+    }
+
+}
---
+++ tests/assignments/triangle_ex/TriangleTests.java
@@ -0,0 +1,15 @@
+package assignments.triangle_ex;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+/**
+ * Created by RachelSmith on 09/09/2016.
+ */
+public class TriangleTests {
+    @Test
+    public void printsOnlyOneStar() throws Exception {
+        assertEquals("*", new Triangle().justOneStar());
+    }
+}
